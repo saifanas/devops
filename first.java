5640e1d63aa3970e453b05adf6974ae54a5040d6
anas@11 <anas@bots.in>
2023-07-19 18:06:29 +0530

    Added a first programm in a : first.java

--- first.java
+++ first.java
@@ -8,3 +8,11 @@ second line is added to a programm...
 third line is added to a programm..
 
 but i want to add another programm..
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
